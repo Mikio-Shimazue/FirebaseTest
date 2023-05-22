e032d79debb5b835c077f021809868775be7f8ae
Mikio-Shimazue <shimazun@icloud.com>
2023-05-22 20:23:43 +0900

    プロジェクト作成

---
+++ FirebaseTest/FirebaseTest/FirebaseAccsess.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseAccsess.swift
+//  FirebaseTest
+//
+//  Created by Mikizin on 2023/05/22.
+//
+
+import Foundation
---
+++ FirebaseTest/FirebaseTest/FirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestApp.swift
+//  FirebaseTest
+//
+//  Created by Mikizin on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
