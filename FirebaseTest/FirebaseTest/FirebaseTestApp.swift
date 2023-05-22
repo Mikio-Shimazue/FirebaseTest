d9ad77084c0567f5c817daa43143ee63bc0d9460
Mikio-Shimazue <shimazun@icloud.com>
2023-05-22 20:28:22 +0900

    Firebase初期処理追加

--- FirebaseTest/FirebaseTest/FirebaseTestApp.swift
+++ FirebaseTest/FirebaseTest/FirebaseTestApp.swift
@@ -5,13 +5,23 @@
 //  Created by Mikizin on 2023/05/22.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct FirebaseTestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
