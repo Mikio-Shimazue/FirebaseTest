0923a883b61fb24fc374efbb14a2751765ed09cf
Mikio-Shimazue <shimazun@icloud.com>
2023-05-30 21:56:04 +0900

    カメラーロールの画像ピッカービュー対応

--- FirebaseTest/FirebaseTest/ContentView.swift
+++ FirebaseTest/FirebaseTest/ContentView.swift
@@ -8,24 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-          Button("Firebase RealTimeDatabase Test") {
-            print("")
-            let fbRealtimeDatabase = FirebaseAccess()
-            fbRealtimeDatabase.setData()
-          }
-        }
-        .padding()
+  var firebaseAccess = FirebaseAccess()
+  @State var showingPicker = false
+  @State var image: UIImage?
+  var body: some View {
+    VStack {
+      if let image = image {
+        Image(uiImage: image)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+      }
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+      Button("Firebase RealTimeDatabase DataSetTest") {
+        print("")
+
+        firebaseAccess.setData()
+      }
+
+      Button("Firebase Storage SetImage") {
+        showingPicker.toggle()
+      }
+
+      Button("Firebase Storage GetImage") {
+        Image(uiImage: firebaseAccess.loadImage()!)
+      }
     }
+    .padding()
+    .sheet(isPresented: $showingPicker) {
+      ImagePickerView(image: $image, sourceType: .library)
+//      firebaseAccess.setImage()
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ FirebaseTest/FirebaseTest/ImagePickerView.swift
@@ -0,0 +1,67 @@
+//
+//  ImagePickerView.swift
+//  FirebaseTest
+//
+//  Created by Mikizin on 2023/05/30.
+//
+
+import SwiftUI
+import UIKit
+
+struct ImagePickerView: UIViewControllerRepresentable {
+  typealias UIViewControllerType = UIImagePickerController
+  @Environment(\.presentationMode) var presentationMode
+  @Binding var image: UIImage?
+  
+  enum SourceType {
+    case camera
+    case library
+  }
+  
+  var sourceType: SourceType
+  var allowsEditing: Bool = false
+  
+  class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    let parent: ImagePickerView
+    
+    init(_ parent: ImagePickerView) {
+      self.parent = parent
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+      getUIImage(didFinishPickingMediaWithInfo: info)
+    }
+    
+    /// カメラロールで選択した画像をUIIMageに設定する
+    /// - Parameter info: <#info description#>
+    func getUIImage(didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+      if let image = info[.editedImage] as? UIImage {
+        parent.image = image
+      } else if let image = info[.originalImage] as? UIImage {
+        parent.image = image
+      }
+      parent.presentationMode.wrappedValue.dismiss()
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
+  func makeUIViewController(context: Context) -> UIImagePickerController {
+    let viewController = UIImagePickerController()
+    viewController.delegate = context.coordinator
+    switch sourceType {
+    case .camera:
+      viewController.sourceType = UIImagePickerController.SourceType.camera
+    case .library:
+      viewController.sourceType = UIImagePickerController.SourceType.photoLibrary
+    }
+    viewController.allowsEditing = allowsEditing
+    return viewController
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    
+  }
+}
+
